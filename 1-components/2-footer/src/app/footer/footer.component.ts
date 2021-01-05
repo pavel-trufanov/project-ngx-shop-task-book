84f7b371d7e80050e27314ebdd382cb4eeb0c303
pavel <paul.trufanov@gmail.com>
2021-01-06 00:28:26 +0200

    components: footer

--- 1-components/2-footer/src/app/footer/footer.component.ts
+++ 1-components/2-footer/src/app/footer/footer.component.ts
@@ -1,10 +1,19 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import {Component, ChangeDetectionStrategy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'ngx-shop-footer',
   templateUrl: './footer.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+
+  public currentYear: number;
+
+  public author: string;
+
+  ngOnInit(): void {
+    this.currentYear = new Date().getFullYear();
+    this.author = 'Angular Course';
+  }
 
 }
