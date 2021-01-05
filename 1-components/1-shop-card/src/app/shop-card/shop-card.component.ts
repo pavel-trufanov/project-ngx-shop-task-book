56c07c1ee825d965c30f99190ce2ee2e48194c57
pavel <paul.trufanov@gmail.com>
2021-01-05 23:58:46 +0200

    components: shop-card

--- 1-components/1-shop-card/src/app/shop-card/shop-card.component.ts
+++ 1-components/1-shop-card/src/app/shop-card/shop-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {ICartProduct} from '../../../../../shared/mocks/1-components/cart-product';
 
 @Component({
   selector: 'app-shop-card',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
 })
 
 export class ShopCardComponent {
+
+  @Input()
+  public product: ICartProduct;
+
+  @Output()
+  public increment = new EventEmitter();
+
+  @Output()
+  public decrement = new EventEmitter();
+
+  public incrementProductInCart() {
+    this.increment.emit();
+  }
+
+  public decrementProductInCart() {
+    this.decrement.emit();
+  }
 
 }
