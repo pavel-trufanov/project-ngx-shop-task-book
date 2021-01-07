607978c93724b3d61b72258ccd84ecca84bf7d49
pavel <paul.trufanov@gmail.com>
2021-01-07 23:54:28 +0200

    components: header

--- 1-components/5-header/src/app/header/header.component.ts
+++ 1-components/5-header/src/app/header/header.component.ts
@@ -1,10 +1,23 @@
-import { Component, ChangeDetectionStrategy} from '@angular/core';
+import {Component, ChangeDetectionStrategy, Output, EventEmitter, OnInit} from '@angular/core';
 
 @Component({
   selector: 'ngx-shop-header',
   templateUrl: './header.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  public counter;
+
+  @Output()
+  public goToBasket = new EventEmitter();
+
+  ngOnInit(): void {
+    this.counter = 5;
+  }
+
+  public redirectTo() {
+    this.goToBasket.emit();
+  }
 
 }
